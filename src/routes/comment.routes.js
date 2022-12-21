9b648f8d02e4015fb40c331d099ada814c427085
Aline096 <anoclez@gmail.com>
2022-12-21 23:10:12 +0200

    Models,Controllers,Routes,refactoring to ES6 and authentication

--- src/routes/comment.routes.js
+++ src/routes/comment.routes.js
@@ -1,6 +1,6 @@
 import express from 'express'
 const router = express.Router();
-const { getComments, postComment } = require('../controllers/comment.controller');
+import { getComments, postComment } from "../controllers/comment.controller";
 
 router.get('/:id', getComments)
 router.post('/:id', postComment)
