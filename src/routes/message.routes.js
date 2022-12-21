23e9662ca985be31e69446414943b1445c0b7e59
Aline096 <anoclez@gmail.com>
2022-12-21 23:10:12 +0200

    Models,Controllers,Routes,refactoring to ES6 and authentication

--- src/routes/message.routes.js
+++ src/routes/message.routes.js
@@ -1,12 +1,12 @@
-const express = require('express');
+import express from 'express';
 const router = express.Router();
-const auth = require('../middlewares/authMiddleware')
-const {createMessage, getMessages, getMessage, deleteMessage} = require('../controllers/message.controller');
+import auth from '../middlewares/authMiddleware';
+import { createMessage, getMessages, getMessage, deleteMessage } from "../controllers/message.controller";
 
 
-router.get('/', getMessages);
-router.get('/:id', getMessage)
+router.get('/',auth, getMessages);
+router.get('/:id',auth, getMessage)
 router.post('/', createMessage);
-router.delete('/:id', deleteMessage);
+router.delete('/:id',auth, deleteMessage);
 
 module.exports = router;
