349cbc75d6fba9b1f5e4057f842536f68739bfb7
Uwera Aline <anoclez@gmail.com>
2023-01-02 20:11:27 +0200

    add proc file to deploy

---
+++ src/validations/article/article.schemas.js
@@ -0,0 +1,28 @@
+import joi from 'joi'
+
+export const createArticleSchema = joi.object({
+    email: joi.string().required().email().messages({
+        'string.base': 'Email must be a string',
+        'string.required': 'Email is a required attribute',
+        'string.email': 'Email must be a valid email string'
+    }),
+
+    password: joi.string().required().messages({
+        'string.base': 'Password must be a string',
+        'string.required': 'Password is a required attribute',
+    })
+})
+
+
+export const updateArticleSchema = joi.object({
+    email: joi.string().required().email().messages({
+        'string.base': 'Email must be a string',
+        'string.required': 'Email is a required attribute',
+        'string.email': 'Email must be a valid email string'
+    }),
+
+    password: joi.string().required().messages({
+        'string.base': 'Password must be a string',
+        'string.required': 'Password is a required attribute',
+    })
+})
