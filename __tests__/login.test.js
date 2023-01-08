da46080fa9c18d8a71d515e34d90b8292827c070
Uwera Aline <anoclez@gmail.com>
2023-01-08 21:05:06 +0200

    finished the testing environment

--- __tests__/login.test.js
+++ __tests__/login.test.js
@@ -13,4 +13,16 @@ describe('Welcome Endpoints', () => {
     expect(res.statusCode).toEqual(200)
     expect(res.body.message).toEqual("Success")
   })
+  it('Should Not Login the admin', async () => {
+    const res = await request(app)
+      .post('/api/user/signin')
+      .send(
+        {
+            email:"admin@gmail.com",
+            password:"Invalid@PAss"
+        }
+      )
+    expect(res.statusCode).toEqual(401)
+    expect(res.body.message).toEqual("Invalid credentials")
+  })
 })
